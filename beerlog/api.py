9c379ce0da81a66e7402ecc617c6ca58f5c10aba
Rafael Cintra <ra.cintra@gmail.com>
2022-04-27 15:51:09 -0300

    Function Test API

--- beerlog/api.py
+++ beerlog/api.py
@@ -1,5 +1,5 @@
-from typing import List
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Response, status
 from beerlog.core  import get_beers_from_database
 from beerlog.serializers import BeerOut, BeerIn
 from beerlog.models import Beer
@@ -7,16 +7,20 @@ from beerlog.database import get_session
 
 api = FastAPI(title="Beerlog")
 
-@api.get("/beers/", response_model=List[BeerOut])
-async def list_beers():
-    beers = get_beers_from_database()
+@api.get("/beers", response_model=List[BeerOut])
+async def list_beers(style: Optional[str] = None):
+    """Lists beers from the database"""
+    beers = get_beers_from_database(style)
     return beers
 
-@api.post("/beers/", response_model=BeerOut)
-async def add_beer(beer_in: BeerIn):
+
+@api.post("/beers", response_model=BeerOut)
+async def add_beer(beer_in: BeerIn, response: Response):
     beer = Beer(**beer_in.dict())
     with get_session() as session:
         session.add(beer)
         session.commit()
         session.refresh(beer)
+
+        response.status_code = status.HTTP_201_CREATED
     return beer
